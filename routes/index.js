0e7966f200031079feb892255a8e1472ed52cab8
Cedric-Z <514074577@qq.com>
2016-08-26 23:25:36 -0400

    增加rankpage

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.redirect('/create');
 });
+router.get('/rank', function(req, res, next) {
+  res.render('rank')
+});
+
 
 module.exports = router;
